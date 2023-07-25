f4de9f148cfdd2d471bfd9387c179f9e406c354d
Rémi Plante <REPLA53@ulaval.ca>
2023-07-25 19:15:56 -0400

    small modifs

--- src/components/SynthController.py
+++ src/components/SynthController.py
@@ -1,4 +1,5 @@
 import pyaudio
+import numpy as np
 
 from NotesController import NotesController
 
@@ -6,9 +7,20 @@ class SynthController:
     def __init__(self, oscillator, bufferSize= 16):
         self.oscillator = oscillator
         self.bufferSize = bufferSize
+        self.NotesController = NotesController()
 
         p = pyaudio.PyAudio()
         self.stream = p.open(format=pyaudio.paFloat32, channels=1, rate=44100, output=True, frames_per_buffer=self.bufferSize)
+
+
+    def get_samples(self, generator, buffer_size):
+        samples = [next(generator) for _ in range(buffer_size)]
+        return np.array(samples)
+
+
+    def play_sound(self, generator):
+        samples = self.get_samples(generator, self.bufferSize)
+        self.stream.write(samples.astype(np.float32).tobytes())
 
 
     def play():
