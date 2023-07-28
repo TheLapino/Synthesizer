4e22e71e6c3357d8f3c35b1e8b06e22bd3c10a91
Rémi Plante <REPLA53@ulaval.ca>
2023-07-28 10:44:07 -0400

    Cleaned up main.py

--- src/main.py
+++ src/main.py
@@ -39,46 +39,5 @@ def main():
 
 
 
-def get_samples(generator, buffer_size=BUFFER_SIZE):
-    samples = [next(generator) for _ in range(buffer_size)]
-    return np.array(samples)
-
-
-def play_sound(generator):
-    samples = get_samples(generator)
-    stream.write(samples.astype(np.float32).tobytes())
-
-
-def visualise(signal):
-    t = np.linspace(0, 2, int(SAMPLE_RATE * DURATION), endpoint=False)
-    visualiseSignal(t, signal)
-
-
-def normalizeSignal(signal):
-    max_amplitude = max(abs(signal))
-    signal = signal / max_amplitude
-    return signal
-
-
-def saveWaveFile(signal):
-    wav_file=wave.open("test_audio.wav","w")
-
-    nchannels = 1
-    sampwidth = 2
-    nframes = len(signal)
-    comptype = "NONE"
-    compname = "not compressed"
-    wav_file.setparams((nchannels, sampwidth, SAMPLE_RATE, nframes, comptype, compname))
-
-    for sample in signal:
-        wav_file.writeframes(struct.pack('h', int( sample * 32767.0 )))
-
-    wav_file.close()
-
-
-def sayHi(key):
-    print(key)
-
-
 if __name__ == "__main__":
     main()
